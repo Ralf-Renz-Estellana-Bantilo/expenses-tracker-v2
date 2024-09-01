f30f3dc5d0a0945a32261622ee4a86ff441cb269
Ralf-Renz-Estellana-Bantilo <ralfrenzbantilo853@gmail.com>
2024-09-01 13:32:23 +0800

    Added search page and its basic functionalities

--- app/components/modals/SearchResults.tsx
+++ app/components/modals/SearchResults.tsx
@@ -3,29 +3,44 @@
 import { fetchSearch } from "@/app/controller/controller"
 import useAlert from "@/app/hook/useAlert"
 import { ExpensesType } from "@/app/types/type"
-import React, { useEffect, useRef, useState } from "react"
+import React, { Dispatch, useEffect, useRef, useState } from "react"
 import { CardList } from "../CardList"
 import { formatMoney, getExpenseDescription, getIcons } from "@/app/utils/utils"
 import { AppContext } from "@/app/context/context"
 
-const SearchResults = ({ query }: { query: string }) => {
+const SearchResults = ({
+  query,
+  setResult,
+}: {
+  query: string
+  setResult: Dispatch<React.SetStateAction<ExpensesType[]>>
+}) => {
   const context = AppContext()
   const [data, setData] = useState<ExpensesType[]>([])
   const { showAlert } = useAlert()
 
+  const loadingRef = useRef(false)
+
   const fetchData = async () => {
     if (query.length !== 0) {
+      loadingRef.current = true
       try {
         const result = await fetchSearch<ExpensesType[]>({ searchText: query })
 
         setData(result)
+        setResult(result)
+        loadingRef.current = false
       } catch (error) {
         showAlert({
           message: "Error fetching data...",
           type: "error",
         })
         setData([])
+        loadingRef.current = false
       }
+    } else {
+      setData([])
+      setResult([])
     }
   }
 
@@ -34,21 +49,16 @@ const SearchResults = ({ query }: { query: string }) => {
   }
 
   useEffect(() => {
-    ;(async function () {
-      await fetchData()
-    })()
+    fetchData()
   }, [query])
 
-  if (query.length === 0) {
-    return <div>Type to search...</div>
-  }
-
   if (data.length === 0) {
-    return <div>No results found for "{query}"</div>
+    return null
   }
 
   return (
     <div className="flex flex-col">
+      {loadingRef.current && <p>Loading...</p>}
       {data.map((expense) => (
         <CardList
           key={expense.ID}
---
+++ app/search/page.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import React, { ChangeEvent, useCallback, useMemo, useState } from "react"
+import { SearchIcon } from "../icons/icons"
+import { Checkbox, Input } from "@nextui-org/react"
+import { CardList } from "../components/CardList"
+import { formatMoney } from "../utils/utils"
+import {
+  Wrapper,
+  WrapperContent,
+  WrapperFooter,
+  WrapperHeader,
+} from "../components/Wrapper"
+import useDelay from "../hook/useDelay"
+import SearchResults from "../components/modals/SearchResults"
+import { ExpensesType } from "../types/type"
+
+const SearchPage = () => {
+  const [query, setQuery] = useState("")
+  const [result, setResult] = useState<ExpensesType[]>([])
+  const deferredQuery = useDelay(query, 1000)
+
+  const handleInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value.trimStart())
+  }, [])
+
+  const totalExpenses: number = useMemo(() => {
+    return (
+      result?.reduce(
+        (accumulator, item) => Number(accumulator) + Number(item.amount),
+        0
+      ) ?? 0
+    )
+  }, [result])
+
+  return (
+    <div className="flex flex-col gap-2">
+      <Wrapper>
+        <WrapperHeader>
+          <div className="flex flex-col gap-2">
+            <div className="flex">
+              <Input
+                variant="bordered"
+                label="Search Expenses"
+                autoFocus
+                isClearable
+                onClear={() => setQuery("")}
+                value={query}
+                radius="lg"
+                onChange={handleInputChange}
+                placeholder="Type to search..."
+                startContent={<SearchIcon />}
+              />
+            </div>
+            {query && (
+              <div className="flex items-center justify-between">
+                <span>Results Found:</span>
+                <b>{result.length}</b>
+              </div>
+            )}
+          </div>
+        </WrapperHeader>
+        <WrapperContent scrollable maxScrollableHeight="70vh">
+          <SearchResults query={deferredQuery} setResult={setResult} />
+        </WrapperContent>
+        {result.length > 0 && (
+          <WrapperFooter className="flex items-center justify-between">
+            <h3 className="text-default-500">Total:</h3>
+            <p className="text-default-500"> {formatMoney(totalExpenses)}</p>
+          </WrapperFooter>
+        )}
+      </Wrapper>
+    </div>
+  )
+}
+
+export default SearchPage
